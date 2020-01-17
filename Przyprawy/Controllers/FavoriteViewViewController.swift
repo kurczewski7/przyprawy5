d452ea0ccde2066c36b028fa1323063414c73605
Kurczewski Slawomir <slawomir.kurczewski@gmail.com>
2020-01-17 10:17:15 +0100

    CNContact store added OK

--- Przyprawy/Controllers/FavoriteViewViewController.swift
+++ Przyprawy/Controllers/FavoriteViewViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 import Contacts
 
 class FavoriteViewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-//    var contact = ContactEntry()
-//    var store = CNContactStore()
+    var contacts: [CNContact] = []
+    var store = CNContactStore()
+    struct FavoriteContact {
+        let name: String = "Name"
+    }
     var contactStore = CNContactStore()
     //var contacts = [ContactEntry]()
     
@@ -23,16 +26,51 @@ class FavoriteViewViewController: UIViewController, UITableViewDelegate, UITable
     }
     @available (iOS 9.0, *)
     func getContacts() {
-        let  store = CNContactStore()
-        let predicate: NSPredicate = CNContact.predicateForContacts(matchingName: "ku")
+        isAccesToContact { (success) in
+            self.retriveContacts()
+        }
+        
+        //let  store = CNContactStore()
+        //let predicate: NSPredicate = CNContact.predicateForContacts(matchingName: "ku")
         
         //let xxx  = try store.unifiedContactsMatchingPredicate(CNContact.predicateForContactsMatchingName("ku"),
         //                                                                    keysToFetch:[CNContactGivenNameKey, CNContactFamilyNameKey])
         //let yyy = xxx.CNContact[0]
         
     }
+    func isAccesToContact(_ completion: @escaping (_ success: Bool) -> Void) {
+        let authorizationStatus = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
+        switch authorizationStatus {
+        case .authorized:
+            completion(true)
+        case .denied, .notDetermined:
+            self.contactStore.requestAccess(for: .contacts) { (accessGranted, error) in
+                completion(accessGranted)
+            }
+        default:
+                completion(false)
+         }
+    }
+    func retriveContacts() {
+        let keys  = [CNContactGivenNameKey, CNContactFamilyNameKey,CNContactPhoneNumbersKey]
+        let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+        do {
+            //var favoritContacts = [FavoriteContact]()
+            try store.enumerateContacts(with: request, usingBlock: { (contact, stopPointer) in
+                print("Osoba:")
+                print(contact.givenName)
+                print(contact.familyName)
+                print(contact.phoneNumbers.first?.value.stringValue ?? "brak tel")
+                self.contacts.append(contact)
+            })
+           // let names = ExpandableNames()
+        } catch let err {
+                print("Failed to enumerate contacts", err)
+        }
+        
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8
+        return contacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,8 +80,8 @@ class FavoriteViewViewController: UIViewController, UITableViewDelegate, UITable
          //contact = contactList[indexPath.row]
 
         //cell.textLabel?.text = "\(contact.givenName) \(contact.familyName)"
-        cell.textLabel?.text="AAAA"
-        cell.detailTextLabel?.text="bbb"
+        cell.textLabel?.text = contacts[indexPath.row].familyName
+        cell.detailTextLabel?.text = contacts[indexPath.row].givenName
         
         
         return cell
