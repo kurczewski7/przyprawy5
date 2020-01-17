cf8af9a4fb7c4b274ca13b9c961c9bd5a54d024e
Kurczewski Slawomir <slawomir.kurczewski@gmail.com>
2020-01-17 01:10:02 +0100

    contact error

--- Przyprawy/Controllers/FavoriteViewViewController.swift
+++ Przyprawy/Controllers/FavoriteViewViewController.swift
@@ -7,26 +7,50 @@
 //
 
 import UIKit
+import Contacts
 
 class FavoriteViewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//    var contact = ContactEntry()
+//    var store = CNContactStore()
+    var contactStore = CNContactStore()
+    //var contacts = [ContactEntry]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getContacts()
+        
+        // Do any additional setup after loading the view.
+    }
+    @available (iOS 9.0, *)
+    func getContacts() {
+        let  store = CNContactStore()
+        let predicate: NSPredicate = CNContact.predicateForContacts(matchingName: "ku")
+        
+        //let xxx  = try store.unifiedContactsMatchingPredicate(CNContact.predicateForContactsMatchingName("ku"),
+        //                                                                    keysToFetch:[CNContactGivenNameKey, CNContactFamilyNameKey])
+        //let yyy = xxx.CNContact[0]
+        
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 8
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=UITableViewCell()
+        
+        let contact: CNContact!
+         //contact = contactList[indexPath.row]
+
+        //cell.textLabel?.text = "\(contact.givenName) \(contact.familyName)"
         cell.textLabel?.text="AAAA"
         cell.detailTextLabel?.text="bbb"
+        
+        
         return cell
     }
     
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
 
     /*
     // MARK: - Navigation
