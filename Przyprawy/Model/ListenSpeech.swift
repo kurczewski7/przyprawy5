80279c54a28b831bf6ea8c56710aa268accbe235
Kurczewski Slawomir <slawomir.kurczewski@gmail.com>
2020-02-03 17:36:15 +0100

    ListenSpeak next.

--- Przyprawy/Model/ListenSpeech.swift
+++ Przyprawy/Model/ListenSpeech.swift
@@ -10,11 +10,34 @@ import Foundation
 import Speech
 
 class ListenSpeech {
-     //var memoData: [Memo]!
-       var currentLanguage = 0
-       let languaeList = ["pl","en_GB","de","fr_FR","es_ES"]
-       
-       lazy var speechRecognizer: SFSpeechRecognizer? = nil
+    struct Memo {
+        let memoTitle: String
+        let memoDate: Date
+        let memoText: String
+    }
+    //memoTitle: "Nowe nagranie", memoDate: Date(), memoText: self.recordedMessage.text!
+    //typealias Memo = String
+    var memoData: [Memo]!
+    
+    var currentLanguage = 0
+    let languaeList = ["pl","en_GB","de","fr_FR","es_ES"]
+    var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    var recognitionTask: SFSpeechRecognitionTask?
+    lazy var speechRecognizer: SFSpeechRecognizer? = nil
+    //----------
+    var recordedMessage: String = ""
+    var isRecordEnabled = true
+
+    var languageId: String  {
+        get {
+         return Setup.languageId
+        }
+    }
+    lazy var audioEngine: AVAudioEngine = {
+           let audioEngine = AVAudioEngine()
+           return audioEngine
+       }()
+
     //    lazy var speechRecognizer: SFSpeechRecognizer? = {
     //        if let recognizer = SFSpeechRecognizer(locale: Locale(identifier: languaeList[currentLanguage])) {
     //            recognizer.delegate = self
@@ -31,31 +54,14 @@ class ListenSpeech {
                 return nil
             }
         }
-        lazy var audioEngine: AVAudioEngine = {
-            let audioEngine = AVAudioEngine()
-            return audioEngine
-        }()
         
-        var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-        var recognitionTask: SFSpeechRecognitionTask?
-
+ 
          func Load() {
 
-//            // request auth
-//            self.requestAuth()
-//            self.recordedMessage.delegate = self
-//
-//            // init data
-//            memoData = []
-//
-//            // tableview delegations
-//            self.tableView.delegate = self
-//            self.tableView.dataSource = self
-//
-//            // hide recording views
-//            self.recordingView.isHidden = true
-//            self.fadedView.isHidden = true
-         
+            // request auth
+            self.requestAuth()
+            // init data
+            memoData = []
         }
 
         func requestAuth() {
@@ -78,28 +84,27 @@ class ListenSpeech {
 //            currentLanguage = sender.selectedSegmentIndex
 //            print("lan=\(currentLanguage)")
 //        }
-//        @IBAction func didTapRecordButton(sender: UIButton) {
-//            self.speechRecognizer = setupSpeechRecognizer()
-//            if audioEngine.isRunning {
-//                audioEngine.stop()
-//                recognitionRequest?.endAudio()
-//            } else {
-//                self.startRecording()
+         func didTapRecordButton() {
+            self.speechRecognizer = setupSpeechRecognizer()
+            if audioEngine.isRunning {
+                audioEngine.stop()
+                recognitionRequest?.endAudio()
+            } else {
+                self.startRecording()
 //                self.recordingView.isHidden = false
 //                self.fadedView.alpha = 0.0
 //                self.fadedView.isHidden = false
 //                UIView.animate(withDuration: 1.0) {
 //                    self.fadedView.alpha = 1.0
-//                }
-//            }
-//        }
-//        @IBAction func stopRecording(sender: UIButton) {
-//            if audioEngine.isRunning {
-//                audioEngine.stop()
-//                recognitionRequest?.endAudio()
-//                audioEngine.inputNode.removeTap(onBus: 0)
-//                self.memoData.append(Memo(memoTitle: "Nowe nagranie", memoDate: Date(), memoText: self.recordedMessage.text!))
-//
+                }
+        }
+        func stopRecording() {
+            if audioEngine.isRunning {
+                audioEngine.stop()
+                recognitionRequest?.endAudio()
+                audioEngine.inputNode.removeTap(onBus: 0)
+                self.memoData.append(Memo(memoTitle: "Nowe nagranie", memoDate: Date(), memoText: self.recordedMessage))
+
 //                UIView.animate(withDuration: 0.5, animations: {
 //                    self.fadedView.alpha = 0.0
 //                }) { (finished) in
@@ -107,73 +112,56 @@ class ListenSpeech {
 //                    self.recordingView.isHidden = true
 //                    self.tableView.reloadData()
 //                }
-//            }
-//            self.speechRecognizer = nil
-//        }
-//
-//        func startRecording() {
-//
-//            if let recognitionTask = self.recognitionTask {
-//                recognitionTask.cancel()
-//                self.recognitionTask = nil
-//            }
-//
-//            self.recordedMessage.text = ""
-//
-//            let audioSession = AVAudioSession.sharedInstance()
-//            do {
-//                try audioSession.setCategory(AVAudioSessionCategoryRecord)
-//                try audioSession.setMode(AVAudioSessionModeMeasurement)
-//                try audioSession.setActive(true, with: AVAudioSessionSetActiveOptions.notifyOthersOnDeactivation)
-//            }catch {
-//                print(error)
-//            }
-//
-//            recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-//
-//            guard let recognitionRequest = self.recognitionRequest else {
-//                fatalError("Niemożliwe utworzenie bufora dźwięku") //Unable to create a speech audio buffer
-//            }
-//
-//            recognitionRequest.shouldReportPartialResults = true
-//            recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
-//
-//                var isFinal = false
-//                if let result = result {
-//                    let sentence = result.bestTranscription.formattedString
-//                    self.recordedMessage.text = sentence
-//                    isFinal = result.isFinal
-//                }
-//
-//                if error != nil || isFinal {
-//                    self.audioEngine.stop()
-//                    self.audioEngine.inputNode.removeTap(onBus: 0)
-//                    self.recognitionRequest = nil
-//                    self.recognitionTask = nil
-//                    self.recordButton.isEnabled = true
-//                }
-//
-//            })
-//
-//            let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
-//            audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
-//                self.recognitionRequest?.append(buffer)
-//            }
-//
-//            audioEngine.prepare()
-//            do{
-//                try audioEngine.start()
-//            }catch {
-//                print(error)
-//            }
-//        }
-//
-//    }
+            }
+            self.speechRecognizer = nil
+        }
+
+        func startRecording() {
+            if let recognitionTask = self.recognitionTask {
+                recognitionTask.cancel()
+                self.recognitionTask = nil
+            }
+            self.recordedMessage = ""
+            let audioSession = AVAudioSession.sharedInstance()
+            do {
+                try audioSession.setCategory(AVAudioSession.Category.record)
+                try audioSession.setMode(AVAudioSession.Mode.measurement)
+                try audioSession.setActive(true, options: AVAudioSession.SetActiveOptions.notifyOthersOnDeactivation)
+                //try audioSession.setActive(true, with: AVAudioSession.SetActiveOptions.notifyOthersOnDeactivation)
+            }catch {
+                print(error)
+            }
+            recognitionRequest = SFSpeechAudioBufferRecognitionRequest()             //   Unable to create a speech audio buffer
+            guard let recognitionRequest = self.recognitionRequest else {    fatalError("Niemożliwe utworzenie bufora dźwięku")      }
+            recognitionRequest.shouldReportPartialResults = true
+            recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+                var isFinal = false
+                if let result = result {
+                    let sentence = result.bestTranscription.formattedString
+                    self.recordedMessage = sentence
+                    isFinal = result.isFinal
+                }
+                if error != nil || isFinal {
+                    self.audioEngine.stop()
+                    self.audioEngine.inputNode.removeTap(onBus: 0)
+                    self.recognitionRequest = nil
+                    self.recognitionTask = nil
+                    self.isRecordEnabled = true
+                    //self.recordButton.isEnabled = true
+                }
+            })
+            let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
+            audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in   self.recognitionRequest?.append(buffer)  }
+            audioEngine.prepare()
+            do {    try audioEngine.start()   }
+            catch {   print(error)     }
+        }
+// end start
+
 
 //    extension ViewController: SFSpeechRecognizerDelegate {}
-//
 //    extension ViewController: UITableViewDelegate {}
-//
+
 //    extension ViewController: UITableViewDataSource {
 //        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //            return memoData.count
